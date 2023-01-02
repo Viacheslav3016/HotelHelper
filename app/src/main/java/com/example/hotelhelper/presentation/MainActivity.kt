1491cd95f46f1623dee2245280ce33259b6a517d
Viacheslav3016 <wrepleys91@gmail.com>
2023-01-02 10:09:32 +0200

    added mvvm layers

--- app/src/main/java/com/example/hotelhelper/presentation/MainActivity.kt
+++ app/src/main/java/com/example/hotelhelper/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.hotelhelper
+package com.example.hotelhelper.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hotelhelper.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
