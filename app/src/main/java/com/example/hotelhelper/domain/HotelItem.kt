a4c1b89d9f5bb774b92009ca1d7eeb61dfc623e5
Viacheslav3016 <wrepleys91@gmail.com>
2023-01-05 17:11:24 +0200

    Finished main screen and item layout. I add "size" to the HotekItem

--- app/src/main/java/com/example/hotelhelper/domain/HotelItem.kt
+++ app/src/main/java/com/example/hotelhelper/domain/HotelItem.kt
@@ -3,6 +3,7 @@ package com.example.hotelhelper.domain
 data class HotelItem(
     val name:String,
     val count:Int,
+    val size:String,
     val status:Boolean,
     var id:Int = UNDEFIND_ID
 ){
