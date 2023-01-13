d4ce55572631f995c24d20a287223b1d5d5e678a
Viacheslav3016 <wrepleys91@gmail.com>
2023-01-13 12:18:17 +0200

    Create_OrderItemActivity_screen

---
+++ app/src/main/java/com/example/hotelhelper/presentation/OrderItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hotelhelper.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hotelhelper.R
+
+class OrderItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hotel_item)
+    }
+}
